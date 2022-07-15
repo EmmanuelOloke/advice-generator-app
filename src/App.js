49b920f0cfe290468e56fc8a7b5bce0c2c530c13
Emmanuel Oloke <dapopeemmanuel@gmail.com>
2022-07-15 03:58:50 +0100

    initial html

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './App.css';
+import patternDivider from './images/pattern-divider-mobile.svg';
+import diceIcon from './images/icon-dice.svg';
 
 function App() {
-  return <div>Everything Clean and nice!</div>;
+  return (
+    <main className="main-content">
+      <p className="advice-header">ADVICE #117</p>
+      <p className="advice">
+        "It is easy to sit up and take notice, what's difficult is getting up
+        and taking action."
+      </p>
+      <img className="divider" src={patternDivider} alt="pattern divider" />
+      <div className="dice-container">
+        <img className="dice" src={diceIcon} alt="dice" />
+      </div>
+    </main>
+  );
 }
 
 export default App;
